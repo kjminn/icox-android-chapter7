d42ee007d38b76ceceb052e1c0215caabe621c96
akj61300 <rnlwjd0613@gmail.com>
2018-12-18 18:20:36 +0900

    Preference 예제 UI 추가

---
+++ app/src/main/java/com/akj/quizlocker/PrefExActivity.kt
@@ -0,0 +1,12 @@
+package com.akj.quizlocker
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class PrefExActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pref_ex)
+    }
+}
