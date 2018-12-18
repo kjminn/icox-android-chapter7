3b10333c46dba21522cbe3981f76b6ed03c8ab70
akj61300 <rnlwjd0613@gmail.com>
2018-12-18 19:28:35 +0900

    퀴즈 데이터 연동

--- app/src/main/java/com/akj/quizlocker/QuizLockerActivity.kt
+++ app/src/main/java/com/akj/quizlocker/QuizLockerActivity.kt
@@ -3,11 +3,16 @@ package com.akj.quizlocker
 import android.app.KeyguardManager
 import android.content.Context
 import android.os.Build
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_quiz_locker.*
+import org.json.JSONArray
+import org.json.JSONObject
+import java.util.*
 
 class QuizLockerActivity : AppCompatActivity() {
+    var quiz: JSONObject? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // 잠금화면보다 상단에 위치하기 위한 설정 조정. 버전별로 사용법이 다르기 때문에 버전에 따라 적용
@@ -26,5 +31,14 @@ class QuizLockerActivity : AppCompatActivity() {
         // 화면을 켜진 상태로 유지
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
         setContentView(R.layout.activity_quiz_locker)
+        // 퀴즈 데이터를 가져온다.
+        val json = assets.open("capital.json").reader().readText()
+        val quizArray = JSONArray(json)
+        // 퀴즈를 선택한다.
+        quiz = quizArray.getJSONObject(Random().nextInt(quizArray.length()))
+        // 퀴즈를 보여준다.
+        quizLabel.text = quiz?.getString("question")
+        choice1.text = quiz?.getString("choice1")
+        choice2.text = quiz?.getString("choice2")
     }
 }
