48a1108a7f8cdddad86486b0dbba092086157947
akj61300 <rnlwjd0613@gmail.com>
2018-12-18 18:07:07 +0900

    외부저장소에 파일저장

--- app/src/main/java/com/akj/quizlocker/FileExActivity.kt
+++ app/src/main/java/com/akj/quizlocker/FileExActivity.kt
@@ -1,12 +1,17 @@
 package com.akj.quizlocker
 
 import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
 import android.support.v7.app.AppCompatActivity
 import android.text.TextUtils
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_file_ex.*
+import java.io.File
+import java.io.FileInputStream
 import java.io.FileNotFoundException
+import java.io.FileOutputStream
 
 class FileExActivity : AppCompatActivity() {
     // 데이터 저장에 사용할 파일이름
@@ -24,9 +29,14 @@ class FileExActivity : AppCompatActivity() {
                 TextUtils.isEmpty(text) -> {
                     Toast.makeText(applicationContext, "텍스트가 비어있습니다.", Toast.LENGTH_LONG).show()
                 }
+                !isExternalStorageWritable() -> {
+                    Toast.makeText(applicationContext, "외부 저장장치가 없습니다.", Toast.LENGTH_LONG).show()
+                }
                 else -> {
                     // 내부 저장소 파일에 저장하는 함수 호출
-                    saveToInnerStorage(text, filename)
+                    //saveToInnerStorage(text, filename)
+                    // 외부 저장소 파일에 저장하는 함수 호출
+                    saveToExternalStorage(text, filename)
                 }
             }
         }
@@ -34,7 +44,9 @@ class FileExActivity : AppCompatActivity() {
         loadButton.setOnClickListener {
             try {
                 // textField 의 텍스트를 불러온 텍스트로 설정한다.
-                textField.setText(loadFromInnerStorage(filename))
+                // textField.setText(loadFromInnerStorage(filename))
+                // 외부저장소 앱전용 디렉토리의 파일에서 읽어온 데이터로 textField 의 텍스트를 설정
+                textField.setText(loadFromExternalStorage(filename))
             } catch (e: FileNotFoundException) {
                 // 파일이 없는 경우 에러메세지 보여줌
                 Toast.makeText(applicationContext, "저장된 텍스트가 없습니다.", Toast.LENGTH_LONG).show()
@@ -58,5 +70,37 @@ class FileExActivity : AppCompatActivity() {
         val fileInputStream = openFileInput(filename)
         // 파일의 저장된 내용을 읽어 String 형태로 불러온다.
         return fileInputStream.reader().readText()
+    }
+
+    // 외부 저장장치를 사용할 수 있고 쓸수 있는지 체크하는 함수
+    fun isExternalStorageWritable(): Boolean {
+        when {
+            // 외부저장장치 상태가 MEDIA_MONTED 면 사용 가능
+            Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED -> return true
+            else -> return false
+        }
+    }
+    // 외부저장장치에서 앱 전용데이터로 사용할 파일 객체를 반환하는 함수
+    fun getAppDataFileFromExternalStorage(filename: String): File {
+        // KITKAT 버전 부터는 앱전용 디렉토리의 디렉토리 타입 상수인 Environment.DIRECTORY_DOCUMENTS 를 지원
+        val dir = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS)
+        } else {
+            // 하위 버전에서는 직접 디렉토리 이름 입력
+            File(Environment.getExternalStorageDirectory().absolutePath + "/Documents")
+        }
+        // 디렉토리의 경로중 없는 디렉토리가 있다면 생성한다.
+        dir?.mkdirs()
+        return File("${dir.absolutePath}${File.separator}${filename}")
+    }
+    // 외부저장소 앱 전용 디렉토리에 파일로 저장하는 함수
+    fun saveToExternalStorage(text: String, filename: String) {
+        val fileOutputStream = FileOutputStream(getAppDataFileFromExternalStorage(filename))
+        fileOutputStream.write(text.toByteArray())
+        fileOutputStream.close()
+    }
+    // 외부저장소 앱 전용 디렉토리에서 파일 데이터를 불러오는 함수
+    fun loadFromExternalStorage(filename: String): String {
+        return FileInputStream(getAppDataFileFromExternalStorage(filename)).reader().readText()
     }
 }
